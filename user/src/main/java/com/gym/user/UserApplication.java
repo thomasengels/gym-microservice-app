e5378740b8060085015cb50bcb8929952fb40f1a
Thomas Engels <thomas.engels@realdolmen.com>
2020-02-18 20:25:02 +0100

    UserApplication

--- user/src/main/java/com/gym/user/UserApplication.java
+++ user/src/main/java/com/gym/user/UserApplication.java
@@ -2,7 +2,6 @@ package com.gym.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class UserApplication {
--- user/src/main/java/com/gym/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gym.user.repository;
-
-import com.gym.user.domain.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
